9413d0f48566777e2b43087cd1d93faccfa92e39
MJ-davie <MJnotZ0@gmail.com>
2024-11-28 14:15:02 +0900

    필터

--- frontend/src/components/ui/Filter.jsx
+++ frontend/src/components/ui/Filter.jsx
@@ -9,16 +9,58 @@ const StyledFilter = styled.button`
     border-radius: 30px;
     background-color : #dcdcdc;
     color : black;
+
+    ${props => {
+        switch(props.variant){
+            //매너온도 1단계(0-12.5)
+            case 'worst' : 
+                return`
+                    background-color : #a9a9a9;
+                    color: #696969;
+                `;
+
+            //매너온도 2단계(12.5-30)
+            case 'bad' :
+                return`
+                    background-color : #4682b4;
+                    color: #191970;
+                `;   
+                
+            //매너온도 3단계(기본)(30-41)
+            case 'defTemp' :
+                return`
+                    background-color : skyblue;
+                    color: dodgerblue;
+                `;
+            
+            //매너온도 4단계(41-42)
+            case 'warm' :
+                return`
+                    background-color : mediumaquamarine;
+                    color: mediumseagreen;
+                `;
+            
+            //매너온도 5단계(42-50)
+            case 'good' :
+                return`
+                    background-color : #f4a460;
+                    color : #ff8c00; 
+                `;
+
+            //매너온도 6단계(50-99)
+            case 'hot' :
+                return`
+                    background-color : coral;
+                    color : orangered;
+                `;
+        }
+    }}
 `;
 
 function Filter(props) {
     const {title, onClick} = props;
-    return (
-        
-            <StyledFilter titile={title} onClick={onClick}>{title || "filter"}</StyledFilter>
-      
-    
-    );
+    return  <StyledFilter titile={title} onClick={onClick}>{title || "filter"}</StyledFilter>  ;  
+
 }
 
 export default Filter;
