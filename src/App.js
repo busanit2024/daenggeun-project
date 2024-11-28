e41394850bef859696c734c5e1a9ee4edafb17be
Leeeeee <dltkdgjs002@naver.com>
2024-11-28 11:59:13 +0900

    테스트

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          dddddddfff
+          안녕하세요 리액트 입니다 아아아아아아ㅏ
         </p>
         <a
           className="App-link"
