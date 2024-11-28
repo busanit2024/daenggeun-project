618d3c4964658a291ec79200e93a792558601946
엄채연 EOM CHAE YEON <43158518+sorimchuku@users.noreply.github.com>
2024-11-28 14:11:26 +0900

    주석

--- backend/src/main/java/com/busanit/daenggeunbackend/entity/TestUser.java
+++ backend/src/main/java/com/busanit/daenggeunbackend/entity/TestUser.java
@@ -10,8 +10,11 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Getter
 @Setter
 public class TestUser {
+  //@Id 어노테이션 지정하면 문서 id를 자동으로 생성해주기 때문에, 유저 아이디 같은 사용자 지정 아이디를 따로 만드는게 낫다고 하네요
   @Id
   private String id;
+
+  //@Indexed 어노테이션으로 유니크 제약조건 걸 수 있는 것 확인함!
   @Indexed(unique = true)
   private String userId;
   private String username;
