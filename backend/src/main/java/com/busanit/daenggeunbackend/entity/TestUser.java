92cb046e9e9c5092a79718718bc4ffa45c18f9bd
엄채연 EOM CHAE YEON <43158518+sorimchuku@users.noreply.github.com>
2024-11-28 14:08:34 +0900

    Unique 키 관련 수정 (인덱싱)
    - Unique 키 사용할 수 있습니다

--- backend/src/main/java/com/busanit/daenggeunbackend/entity/TestUser.java
+++ backend/src/main/java/com/busanit/daenggeunbackend/entity/TestUser.java
@@ -3,6 +3,7 @@ package com.busanit.daenggeunbackend.entity;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "user")
@@ -11,5 +12,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class TestUser {
   @Id
   private String id;
+  @Indexed(unique = true)
+  private String userId;
   private String username;
 }
