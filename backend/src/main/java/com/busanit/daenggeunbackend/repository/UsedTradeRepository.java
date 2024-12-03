b32a00a5360f6d627e7567da509a97968580c258
Leeeeee <dltkdgjs002@naver.com>
2024-12-03 16:09:00 +0900

    import 수정

--- backend/src/main/java/com/busanit/daenggeunbackend/repository/UsedTradeRepository.java
+++ backend/src/main/java/com/busanit/daenggeunbackend/repository/UsedTradeRepository.java
@@ -4,5 +4,5 @@ import com.busanit.daenggeunbackend.entity.UsedTrade;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UsedTradeRepository extends MongoRepository<UsedTrade, String> {
-
+    // 쿼리 메서드 후에 추가
 }
