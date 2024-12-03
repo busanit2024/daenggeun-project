bd0a9531e0e998d3361dfd43f5ecb738b44e7717
엄채연 EOM CHAE YEON <43158518+sorimchuku@users.noreply.github.com>
2024-12-03 16:10:47 +0900

    오타 수정

--- backend/src/main/java/com/busanit/daenggeunbackend/entity/UsedTrade.java
+++ backend/src/main/java/com/busanit/daenggeunbackend/entity/UsedTrade.java
@@ -2,7 +2,7 @@ package com.busanit.daenggeunbackend.entity;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.annotation.id;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
