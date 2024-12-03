745de250369a47c7afa9ef5ffd8411e6c1b07744
dduhell <suhyun97a@gmail.com>
2024-12-03 15:35:01 +0900

    동네생활 리스트 페이지 수정

--- backend/src/main/java/com/busanit/daenggeunbackend/repository/CommunityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.busanit.daenggeunbackend.repository;
-
-import com.busanit.daenggeunbackend.entity.Community;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CommunityRepository extends MongoRepository<Community, String> {
-}
