83c462174fa3f85f1cb26d9d438078270f3793d7
MJ-davie <MJnotZ0@gmail.com>
2024-12-06 16:26:20 +0900

    메인페이지>검색바, 위치 설정
    위치설정 모달 적용

--- frontend/src/components/ui/SearchBar.jsx
+++ frontend/src/components/ui/SearchBar.jsx
@@ -1,19 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import RoundFilter from "./RoundFilter";
-import SearchBox from "./MainSearch";
+import MainSearchBox from "./MainSearchBox";
 import styled from "styled-components";
+import LocationSearchModal from "./LocationSearchModal";
 
 const SearchWrapper = styled.div`
   display: flex;
   gap: 16px; 
-  align-items: center; 
+  align-items: center;
+   
+`;
+
+const StyledRoundFilter = styled(RoundFilter)`
+    white-space: nowrap; 
+    
 `;
 
 const SearchBar = () => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [userLocation, setUserLocation] = useState([{ sigungu: "해운대구", emd: "" }]);
+
+    const handleLocationSelect = (selectedLocation) => {
+        if (typeof selectedLocation === 'string') {
+            const [sigungu, emd] = selectedLocation.split(",").map(loc => loc.trim());
+            
+            const locationObject = {
+                sigungu: sigungu,
+                emd: emd
+            };
+
+            setUserLocation([locationObject]);
+            setIsModalOpen(false); 
+        } else {
+            console.error("선택된 위치가 문자열이 아닙니다:", selectedLocation);
+        }
+    };
+
     return (
         <SearchWrapper>
-            <RoundFilter variant="white" title="해운대구" />
-            <SearchBox />
+            <StyledRoundFilter 
+                LocationIcon 
+                variant="location" 
+                title={`${userLocation[0].sigungu}${userLocation[0].emd ? `, ${userLocation[0].emd}` : ''}`} 
+                onClick={() => setIsModalOpen(true)} />
+            {isModalOpen && (
+                <LocationSearchModal onSelect={handleLocationSelect} onClose={() => setIsModalOpen(false)} />
+            )}
+            <MainSearchBox />
         </SearchWrapper>
     );    
 };
