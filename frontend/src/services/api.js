ece48f460e3818f111899ffd9bafe7b42e77191a
James O <yongho0107@gmail.com>
2024-12-06 13:50:46 +0900

    글쓰기, 수정 페이지

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,12 +1,12 @@
-export const fetchAlba = async () => {
-    try {
-      const response = await fetch("http://localhost:8080/api/alba"); // Spring Boot API 주소
-      if (!response.ok) {
-        throw new Error("Failed to fetch Alba data");
-      }
-      return await response.json();
-    } catch (error) {
-      console.error("Error fetching Alba data:", error);
-      return [];
-    }
-  };
+// export const fetchAlba = async () => {
+//     try {
+//       const response = await fetch("http://localhost:8080/api/alba"); // Spring Boot API 주소
+//       if (!response.ok) {
+//         throw new Error("Failed to fetch Alba data");
+//       }
+//       return await response.json();
+//     } catch (error) {
+//       console.error("Error fetching Alba data:", error);
+//       return [];
+//     }
+//   };
