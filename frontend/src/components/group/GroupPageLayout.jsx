f6b8cd53594bcd435528c0eb0070304097800fd7
엄채연 EOM CHAE YEON <43158518+sorimchuku@users.noreply.github.com>
2024-12-16 09:49:17 +0900

    멤버수 상한 오류

--- frontend/src/components/group/GroupPageLayout.jsx
+++ frontend/src/components/group/GroupPageLayout.jsx
@@ -275,6 +275,8 @@ export default function GroupPageLayout(props) {
         setSubmitted(true);
       }
     }
+
+    console.log(group);
   }, [group]);
 
   const routes = [
@@ -326,7 +328,7 @@ export default function GroupPageLayout(props) {
               <SquareFilter title={group.category} variant="tag" />
               {group.ageRange && <SquareFilter title={group.ageRange} variant="tag" />}
             </div>
-            {!isMember && <Button title={submitted ? '가입신청 중' : group.maxMember >= group.members?.length ? '멤버 수 상한에 도달했어요' : '모임 가입하기'} disabled={submitted || group.maxMember >= group.members?.length}  variant="primary" onClick={handleJoinButton} />}
+            {!isMember && <Button title={submitted ? '가입신청 중' : (group.maxMember !== 0 && group.maxMember <= group.members?.length) ? '멤버 수 상한에 도달했어요' : '모임 가입하기'} disabled={submitted || (group.maxMember !== 0 && group.maxMember <= group.members?.length)}  variant="primary" onClick={handleJoinButton} />}
             {isAdmin &&
               <>
                 { group.requireApproval &&  <Button title={`모임 가입 신청 ${getPendingRequests()}건`} onClick={() => navigate(`/group/${group.id}/requests`)} /> }
